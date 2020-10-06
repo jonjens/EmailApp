54485f27c393e596b188ae90fffc50bb773a2e2f
jonjens <jonassenjens@gmail.com>
2020-10-06 12:46:25 +0200

    Add project files.

---
+++ SendEmailBlazor/Models/DisplayPersonModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SendEmailBlazor.Models
+{
+    public class DisplayPersonModel
+    {
+        [Required]
+        [StringLength(15, ErrorMessage = "First Name is too long.")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(15, ErrorMessage = "Last Name is too long.")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+    }
+}
